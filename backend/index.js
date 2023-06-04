366b2f6a1b86aed37b3919c4c5d26b7e6bbc6fd6
thankyouamir <amirkhannamkhana@gmail.com>
2023-06-04 15:39:24 +0530

    integrate

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-const express= require ("express");
-const app= express();
-require('./db/config');
-const user = require('./db/user');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./db/config");
+const user = require("./db/user");
 app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, res) => {
+  let User = new user(req.body);
+  let result = await User.save();
 
-app.post("/register",async (req,res)=>{
-    let User = new user(req.body);
-    let result = await User.save();
-
-    res.send(result);
-})
+  res.send(result); //from here variable name i.e result could be different because it will be passed as an argument in model
+});
 
 app.listen(5000);
--- client/src/component/SignUp.js
+++ client/src/component/SignUp.js
@@ -1,26 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SignUp=()=>{
-    const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const [passwordc,setPasswordc]=useState("");
-    const collectData=()=>{
-        console.warn(name,email,password,passwordc)
-        if (password !==passwordc){
-            console.warn("password do not match");
-        }
-    }
-    return(
-        <div className='signup'>
-            <h1>Register</h1>
-            <input className='inputBox' type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/>
-            <input className='inputBox' type='text' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Email'/>
-            <input className='inputBox' type='password' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter Password'/>
-            <input className='inputBox' type='password' value={passwordc} onChange={(e)=>setPasswordc(e.target.value)} placeholder='Confirm Password'/>
-            <button type='button' onClick={collectData} className='signBtn'>SignUp</button>
-           
-        </div>
-    )
-}
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const collectData = async () => {
+    //   console.warn(name, email, password, passwordc);
+    let result = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    localStorage.setItem("user", JSON.stringify(result));
+    navigate("/");
+  };
+
+  return (
+    <div className="signup">
+      <h1>Register</h1>
+      <input
+        className="inputBox"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Name"
+      />
+      <input
+        className="inputBox"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter Email"
+      />
+      <input
+        className="inputBox"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter Password"
+      />
+
+      <button type="button" onClick={collectData} className="signBtn">
+        SignUp
+      </button>
+    </div>
+  );
+};
 export default SignUp;
