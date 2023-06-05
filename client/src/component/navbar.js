b02d2aad8e576168f1ea65a20415bdd4089a8757
thankyouamir <amirkhannamkhana@gmail.com>
2023-06-05 23:56:38 +0530

    logout function

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -1,22 +1,29 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const navbar = () => {
   const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
   return (
     <div className="navbar">
       <ul>
         <li>
           <Link to="/">Home</Link>
-          <Link to="add">Add Product</Link>
-          <Link to="update">Update Product</Link>
+          <Link to="/add">Add Product</Link>
+          <Link to="/update">Update Product</Link>
 
-          <Link to="profile">Profile</Link>
+          <Link to="/profile">Profile</Link>
 
           {auth ? (
-            <Link to="logout">Logout</Link>
+            <Link onClick={logout} to="/signup">
+              Logout
+            </Link>
           ) : (
-            <Link to="signup">SignUp</Link>
+            <Link to="/signup">SignUp</Link>
           )}
         </li>
       </ul>
