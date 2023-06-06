f387b6c7fcf88156fe2da52c2e0cf45f6b263fab
thankyouamir <amirkhannamkhana@gmail.com>
2023-06-06 22:35:14 +0530

    login api

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,22 @@ app.use(cors());
 app.post("/register", async (req, res) => {
   let User = new user(req.body);
   let result = await User.save();
+  result = result.toObject();
+  delete result.password;
 
   res.send(result); //from here variable name i.e result could be different because it will be passed as an argument in model
 });
-
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No user found" });
+    }
+  } else {
+    res.send({ result: "No user found" });
+  }
+});
 app.listen(5000);
